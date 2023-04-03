ae0c07ead97b17150193611a0860f77ed49f671a
fakhreddin98 <99963603+fakhreddin98@users.noreply.github.com>
2023-04-03 11:11:42 +0200

    Update the_final_code.py

--- Screen viewer/the_final_code.py
+++ Screen viewer/the_final_code.py
@@ -1,7 +1,4 @@
-# Importera nödvändiga bibliotek för att skapa GUI och för att automatisera webbläsaren
-# pip install requests
-# pip install selenium
-# pip install pyautogui
+
 
 import tkinter
 from tkinter import ttk
@@ -14,10 +11,8 @@ import time
 import pyautogui
 import requests
 
-# Förhindra viloläge
 pyautogui.FAILSAFE = False
 
-# URL till råa innehållet i textfilen på GitHub
 
 text = 'https://raw.githubusercontent.com/fakhreddin98/Volvo_Cars_projects/main/Screen%20viewer/links.txt'
 response = requests.get(text)
@@ -34,8 +29,6 @@ def move_mouse():
 
 
 
-# Funktionen clear_item tar bort innehållet i delay_spinbox och Url_entry,
-# och sätter delay_spinbox till standardvärdet "10" igen.
 def clear_item():
     delay_spinbox.delete(0, tkinter.END)
     delay_spinbox.insert(0, "10")
@@ -45,10 +38,6 @@ urls_lists = []
 urls_list  = []
 delay_list = []
 
-# Funktionen "add_item" hämtar url-adressen och fördröjningen från användarens inmatning i gränssnittet
-# Den lägger sedan till url-adressen och fördröjningen i en lista och lägger till det i Treeview-fönstret
-# Den lägger också till url-adressen i en annan lista med bara url-adresser, för senare användning
-# Funktionen anropar sedan "clear_item" för att tömma inmatningsfälten i gränssnittet
 def add_item():
     Url = Url_entry.get()
     Url = Url.replace(" ", "").replace("\n","")
@@ -64,8 +53,6 @@ def add_item():
     clear_item()
 
 
-# Funktionen "remove_url" tar bort den valda URL:en från Treeview och urls_list.
-# Om en URL har valts i Treeview så tas den bort från Treeview och urls_list med hjälp av dess index.
 def remove_url():
     selection = tree.selection()
     if selection:
@@ -75,9 +62,6 @@ def remove_url():
             del urls_list[index]
   
 
-# Funktionen clear_all tar bort alla url:er från treeview-komponenten och rensar inmatningsfälten.
-# clear_item är en annan funktion som rensar delay_spinbox och Url_entry-fälten
-# tree.delete (* tree.get_children ()) tar bort alla barnnoder i trädet (treeview)
 def clear_all():
     clear_item()    
     tree.delete(*tree.get_children())
@@ -108,58 +92,45 @@ def default_list():
         tree.insert('', 0, values=urls)
 
 
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
 
-
-# Funktion för att öppna flera flikar och uppdatera en rapport på varje flik
 def Start():
-    # Hämta en lista med webbadresser
     urls = urls_list
-    # Hämta värdet från en spinnbox och använda det som en förseningstid för att uppdatera rapporterna
     delays = [int(delay) for delay in delay_list] # antar att delay_list är en lista med förseningar för varje URL i urls_list
     
-    # Ange sökvägen till Edge-webbläsarens drivrutin
     edge_path = r"C:\Users\fkabawe\Documents\msedgedriver.exe"
-    
-    # Skriv ut webbadresserna som är sparade i listan 'urls'
+    edge_options = Options()
+    edge_options.add_argument('user-data-dir=C:\\Users\\fakhe\\AppData\\Local\\Microsoft\\Edge\\User Data')
+    edge_options.add_argument('--start-maximized')
+
     print ("här är urls" ,urls)
-    # Skapa en instans av webbläsaren Edge med hjälp av drivrutinen
-    driver = webdriver.Edge(executable_path=edge_path)
+    driver = webdriver.Edge(service=Service(executable_path=edge_path), options=edge_options)
     driver.maximize_window()
-    # Öppna den första webbadressen i listan i en flik
     driver.get(urls[0])
     delay = delays[0]
 
-    # Loopa genom alla webbadresser i listan och öppna dem i nya flikar
     for url, delay in zip(urls[1:], delays[1:]):
         driver.execute_script(f"window.open('{url}')")
         time.sleep(delay)
 
-    # Hämta en lista med flikhandtag
     tab_handles = driver.window_handles
 
-    # Välj den första fliken som ska uppdateras
     current_tab = 0
 
-    # Loopa tills användaren stänger programmet
     while True:
-        # Välj nästa flik i listan med flikhandtag
         current_tab = (current_tab + 1) % len(tab_handles)
         driver.switch_to.window(tab_handles[current_tab])
         move_mouse()
         try:
-            # Vänta i upp till 10 sekunder på att uppdateringsknappen blir klickbar
             refresh_button = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.ID, "reportAppBarRefreshBtn")))
-            # Klicka på uppdateringsknappen
             ActionChains(driver).move_to_element(refresh_button).click(refresh_button).perform()
             print("refresh utförd")
-            # Vänta i 'delay' sekunder innan nästa uppdatering
             time.sleep(delays[current_tab])
 
         except:
-            # Om uppdateringsknappen inte blir klickbar, vänta i 'delay' sekunder innan nästa försök
             
             time.sleep(delays[current_tab])
-            # Fortsätt till nästa iteration av loopen
             continue
 
 
