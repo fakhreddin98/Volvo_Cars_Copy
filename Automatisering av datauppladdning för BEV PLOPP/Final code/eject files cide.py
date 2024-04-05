d7811a3862fd46d407878cd012022401b1b6b1ea
fakhreddin98 <99963603+fakhreddin98@users.noreply.github.com>
2024-04-05 13:05:16 +0200

    Update and rename eject files cide to eject files cide.py

---
+++ Automatisering av datauppladdning för BEV PLOPP/Final code/eject files cide.py
@@ -0,0 +1,12 @@
+def eject_usb_drive(source_folder):
+    try:
+        logging.info(f"Ejecting USB drive: {source_folder[:2]}")
+        os.system(f'powershell "$driveEject = New-Object -comObject Shell.Application; $driveEject.Namespace(17).ParseName(\\"{source_folder[:2]}\\").InvokeVerb(\\"Eject\\")"')
+        print(f"device{source_folder[:2]} disconnected")
+    except Exception as e:
+        logging.error(f"An error occurred while ejecting USB drive: {e}")
+        print(f"An error occurred while ejecting USB drive: {e}")
+
+
+        eject_usb_drive(source_folder)
+
